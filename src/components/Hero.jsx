968f981ea57bd20d484741f1c32ac2978c6774f3
Denis <denis.shagim@icloud.com>
2024-12-09 11:47:49 +0300

    feat: install gsap for animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -83,11 +83,16 @@ const Hero = () => {
               id="watch-trailer"
               title="Watch Trailer"
               leftIcon={<TiLocationArrow />}
-              containerClass='bg-yellow-300 flex-center gap-1'
+              containerClass="bg-yellow-300 flex-center gap-1"
             />
           </div>
         </div>
       </div>
+
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+        G<b>a</b>ming
+      </h1>
+
     </div>
   );
 };
